dfb8926d896acac8bebbc081f338ad81c9b276af
Andrew Wasielewski <awwasielewski@gmail.com>
2022-01-03 11:39:08 -0500

    snow day :)

--- sorting.py
+++ sorting.py
@@ -3,39 +3,60 @@ from random import sample
 
 pygame.init()
 FPS = 60
-HEIGHT, WIDTH = 600,1228
+HEIGHT, WIDTH = 600,1400
 COLOR = (0,147,255)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("sorting algos")
 
-def drawRects():
-    WIN.fill((0,0,0))
-    x = 2
-    j = 0
-    height = sample(range(590), 175)
-    height.sort()
-    for i in range(175):
-        pygame.draw.rect(WIN, COLOR, pygame.Rect(x, 600 - height[j], 5, height[j]))
-        top = height[j]
-        j += 1
-        x += 7
-    pygame.display.update()
+print("\n\nEnter 1 for BubbleSort\nEnter 2 for Quicksort\n")
+#inp = input("What algorithm would you like to use? ")
+#inp.strip()
 
 
 def main():
     run = True
-    run1 = True
     clock = pygame.time.Clock()
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        if run1 == True:
-            drawRects()
-            run1 = False
-    pygame.quit()
+        keys_pressed = pygame.key.get_pressed()
+        if keys_pressed[pygame.K_SPACE]:
+            bubblesort()
+        
 
+def bubblesort():
+    height = sample(range(10,461), 450)
+    n = len(height)
+    x = 2
+    j = 0
+    
+    for i in range(n):
+        for i in range(n):
+            WIN.fill((0,0,0))
+            pygame.draw.rect(WIN, (0,147,255), pygame.Rect(x, 600 - height[j], 2, height[j]))
+            pygame.display.update()
+        x += 4
+        j += 1
+        for q in range(0, n - i - 1):
+            if height[q] > height[q + 1]:
+                height[q], height[q + 1] = height[q + 1], height[q]
+
+
+    
+            
+    print(height)
+
+
+
+
+    
+    #Run bubblesort on the heights list
+
+    #redraw the scene (win.fill and redraw all rects) after each step of bubble sort 
+
+    
 
 
 if __name__ == "__main__":
