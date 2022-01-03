e59fe5550d750903907e74b82d91dba1da5f989b
Andrew Wasielewski <awwasielewski@gmail.com>
2022-01-03 12:44:05 -0500

    all hail geeks4geeks

--- sorting.py
+++ sorting.py
@@ -2,62 +2,45 @@ import pygame
 from random import sample
 
 pygame.init()
-FPS = 60
-HEIGHT, WIDTH = 600,1400
-COLOR = (0,147,255)
+WIDTH, HEIGHT = 1400,600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("sorting algos")
+pygame.display.set_caption("Sorting algorithms")
+WIDTH = 5
+height = sample(range(20,121), 100)
+x = 14
+run = True
 
-print("\n\nEnter 1 for BubbleSort\nEnter 2 for Quicksort\n")
-#inp = input("What algorithm would you like to use? ")
-#inp.strip()
+def draw(height):
+    for i in range(len(height)):
+        pygame.draw.rect(WIN, (0,147,255), pygame.Rect(x * i, 600 - (height[i] * 5), WIDTH, height[i] * 5))
 
 
-def main():
-    run = True
-    clock = pygame.time.Clock()
-    while run:
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-        keys_pressed = pygame.key.get_pressed()
-        if keys_pressed[pygame.K_SPACE]:
-            bubblesort()
-        
+while run:
+    execute = False
+    pygame.time.delay(10)
+    keys = pygame.key.get_pressed()
 
-def bubblesort():
-    height = sample(range(10,461), 450)
-    n = len(height)
-    x = 2
-    j = 0
-    
-    for i in range(n):
-        for i in range(n):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if keys[pygame.K_SPACE]:
+            execute = True
+
+        if execute == False:
             WIN.fill((0,0,0))
-            pygame.draw.rect(WIN, (0,147,255), pygame.Rect(x, 600 - height[j], 2, height[j]))
-            pygame.display.update()
-        x += 4
-        j += 1
-        for q in range(0, n - i - 1):
-            if height[q] > height[q + 1]:
-                height[q], height[q + 1] = height[q + 1], height[q]
+            draw(height)
+            pygame.display.update
+        if execute == True:
+            for i in range(len(height) - 1):
+                for j in range(len(height) - i - 1):
+                    if height[j] > height[j + 1]:
+                        height[j], height[j + 1] = height[j + 1], height[j]
+                    
+                    WIN.fill((0,0,0))
+                    draw(height)
+                    pygame.time.delay(10)
+                    pygame.display.update()
+
+pygame.quit()
 
 
-    
-            
-    print(height)
-
-
-
-
-    
-    #Run bubblesort on the heights list
-
-    #redraw the scene (win.fill and redraw all rects) after each step of bubble sort 
-
-    
-
-
-if __name__ == "__main__":
-    main()
